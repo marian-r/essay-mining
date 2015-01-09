cda5f1fdc1f5096f84ff775f926fa0624daa4fae
Marian Rusnak <grizly21@gmail.com>
2015-01-09 18:24:17 +0100

    Refactoring of features mining

--- text-features.R
+++ text-features.R
@@ -1,14 +1,4 @@
 
-corpus_foreach <- function(corpus, func) {
-  for(i in 1:length(corpus)) {
-    print(i)
-    doc = corpus[[i]]
-    
-    func(doc)
-       
-  }
-}
-
 annotateSent <- function(string) {
   # Generate an annotator which computes sentence annotations 
   sent_ann <- Maxent_Sent_Token_Annotator()
@@ -24,28 +14,31 @@ annotateWord <- function(string, sentAnnotation) {
 addSentWordData <- function(docData, i, doc) {
   sent_ann = annotateSent(doc)
   word_ann = annotateWord(doc, sent_ann)
+  word_ann = subset(word_ann, type == "word")
   
   sent_ann = as.data.frame(sent_ann)
   word_ann = as.data.frame(word_ann)
   
-  docData[i,"SentCount"] = nrow(sent_ann)
-  docData[i,"SentLen"] = mean(sent_ann$end - sent_ann$start)
-  docData[i,"WordCount"] = nrow(word_ann)
-  docData[i,"WordLen"] = mean(word_ann$end - word_ann$start + 1)
+  sentCount = nrow(sent_ann)
+  sentLen = mean(sent_ann$end - sent_ann$start)
+  wordCount = nrow(word_ann)
+  wordLen = mean(word_ann$end - word_ann$start + 1)
+  
+  data.frame(SentCount = sentCount, SentLen = sentLen, WordCount = wordCount, WordLen = wordLen)
 }
 
 prepareTextFeatures <- function(rawCorpus) {
   docData = data.frame();
-  i = 1
   
-  corpus_foreach(rawCorpus, function(doc) {
-    addSentWordData(docData, i, doc)
+  for(i in 1:length(rawCorpus)) {
+    print(i)
+    doc = rawCorpus[[i]]
+    
+    rowData = addSentWordData(docData, i, doc)
     
     
-    
-    i = i + 1
-  })
-  
+    docData = rbind(docData, rowData)
+  }
   
   docData
 }
