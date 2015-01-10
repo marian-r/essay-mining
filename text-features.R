a4f1e79257b3bce47f4cefc7226eba3056cd52c9
Marian Rusnak <grizly21@gmail.com>
2015-01-10 20:03:52 +0100

    Added POS tagging

--- text-features.R
+++ text-features.R
@@ -1,4 +1,6 @@
 
+library(plyr)
+
 annotateSent <- function(string) {
   # Generate an annotator which computes sentence annotations 
   sent_ann <- Maxent_Sent_Token_Annotator()
@@ -10,11 +12,19 @@ annotateWord <- function(string, sentAnnotation) {
   annotate(string, word_ann, sentAnnotation)
 }
 
+annotatePOS <- function(string, wordAnnotation) {
+  pos_ann <- Maxent_POS_Tag_Annotator()
+  annotate(string, pos_ann, wordAnnotation)
+}
+
 
 addSentWordData <- function(doc) {
   sent_ann = annotateSent(doc)
   word_ann = annotateWord(doc, sent_ann)
+  tag_ann = annotatePOS(doc, word_ann)
+  
   word_ann = subset(word_ann, type == "word")
+  tag_ann = subset(tag_ann, type == "word")
   
   sent_ann = as.data.frame(sent_ann)
   word_ann = as.data.frame(word_ann)
@@ -24,7 +34,13 @@ addSentWordData <- function(doc) {
   wordCount = nrow(word_ann)
   wordLen = mean(word_ann$end - word_ann$start + 1)
   
-  data.frame(SentCount = sentCount, SentLen = sentLen, WordCount = wordCount, WordLen = wordLen)
+  tagCount = table(unlist(tag_ann$features))
+  tagCountDF = as.vector(tagCount)
+  names(tagCountDF) = names(tagCount)
+  
+  vec = c(SentCount = sentCount, SentLen = sentLen, WordCount = wordCount, WordLen = wordLen)
+  # convert to row of a data frame
+  as.data.frame(t(c(vec, tagCountDF)))
 }
 
 prepareTextFeatures <- function(rawCorpus, corpus) {
@@ -42,8 +58,10 @@ prepareTextFeatures <- function(rawCorpus, corpus) {
     doc = rawCorpus[[i]]
     rowData = cbind(rowData, addSentWordData(doc))
     
+    #print(rowData)
     
-    docData = rbind(docData, rowData)
+    # rbind even if columns are different
+    docData = rbind.fill(docData, rowData)
   }
   
   docData
